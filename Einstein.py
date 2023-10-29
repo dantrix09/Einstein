6579bd4a570e2b1279ec49815788c6f02b024388
dantrix09 <danielitox767@gmail.com>
2023-10-28 20:24:22 -0400

    lo arregle

--- Einstein.py
+++ Einstein.py
@@ -1,9 +1,11 @@
 
 def main():
-   x = input("igresaa boludo la masa")
-   masa = float(x)
-   constante = float(299792450^2)
+   x = input("igresaa boludo la masa ")
+   masa = int(x)
+   constante = int(300000000**2)
    E = masa*constante
    print(E)
+
+
 
 main()
